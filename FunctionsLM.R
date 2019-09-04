607a678747d9d832b62cbb6bab3f821c989942f6
Junsouk Choi <jchoi@stat.tamu.edu>
2019-09-03 20:35:56 -0500

    Write the function calculateMSE

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -27,6 +27,7 @@ calculateBeta <- function(X, Y){
 
 # Calculate MSE
 calculateMSE <- function(beta, beta_LS){
+  MSE = crossprod(beta - beta_LS)
   
   # Return MSE - error ||beta - beta_LS||_2^2
   return(MSE)
